bbc2860fa43aaf245dedca715fed5c5d613d0dcb
NikolaiKhrushchov <Khruschov@i.ua>
2026-03-09 18:51:59 +0200

    correction

---
+++ hometasks/src/main/java/hw_2/task_2/TaskWithRows2.java
@@ -0,0 +1,21 @@
+package hw_2.task_2;
+
+public class TaskWithRows2 {
+
+    public static void main(String[] args) {
+        String text = "Testing, is my favourite job";
+        String[] jobs = text.split("\\s+");
+        for (String job : jobs) {
+            System.out.println("Word: " + job + ",Length: " + job.length());
+        }
+        boolean IsFirstLongest = true;
+
+        for (int i = 1; i < jobs.length; i++) {
+            if (jobs[0].length() <= jobs[i].length()) {
+                IsFirstLongest = false;
+                break;
+            }
+        }
+        System.out.println(IsFirstLongest);
+    }
+}
--- hometasks/src/main/java/hw_2/task_3/TaskWithRows3.java
+++ hometasks/src/main/java/hw_2/task_3/TaskWithRows3.java
@@ -1,14 +1,14 @@
 package hw_2.task_3;
 
-public class taskWithRows3 {
+public class TaskWithRows3 {
 
     public static void main(String[] args) {
         String text = "Completely random text in English. In it, we just need to determine how man times the character 'a' occurs there. And we can use the split method and the length method.";
-        char charToCount = 'a';
+        char CharToCount = 'a';
         String lowerCaseString = text.toLowerCase();
-        String[] parts = lowerCaseString.split(String.valueOf(charToCount));
+        String[] parts = lowerCaseString.split(String.valueOf(CharToCount));
         int count = parts.length - 1;
-        System.out.println("Кількість входжень символу 'a' в рядок:" + count);
+        System.out.println("Number of occurrences of the character 'a' in a string: " + count);
     }
 }
 
